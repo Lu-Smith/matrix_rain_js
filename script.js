e534e964a177d2a0b708dc8e0ccc664deac9b245
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-20 15:19:54 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Symbol {
         this.text = this.characters.charAt(Math.floor(Math.random()*this.characters.length));
         context.fillStyle = '#0aff0a';
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-        if (this.y * this.fontSize > this.canvasHeight) {
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
             this.y = 0;
         } else {
             this.y += 1;
@@ -42,12 +42,25 @@ class Effect {
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 15;
+const nextFrame = 1000/fps;
+let timer = 0;
 
-function animate() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.font = effect.fontSize + 'px monospace';
-    effect.symbols.forEach(symbol => symbol.draw(ctx));
+function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if (timer > nextFrame) {
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        ctx.textAlign = 'center';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.font = effect.fontSize + 'px monospace';
+        effect.symbols.forEach(symbol => symbol.draw(ctx));
+        timer = 0;
+    } else {
+        timer += deltaTime;
+    }
+   
     requestAnimationFrame(animate);
 }
-animate();
+animate(0);
