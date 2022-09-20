ec9673e28139554e573841d7faf3821fac4d1c13
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-20 15:46:39 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-gradient.addColorStop(0, 'red');
-gradient.addColorStop(0.2, 'orange');
-gradient.addColorStop(0.4, 'yellow');
-gradient.addColorStop(0.6, 'green');
-gradient.addColorStop(0.8, 'blue');
-gradient.addColorStop(1, 'magenta');
+let gradient = ctx.createRadialGradient(canvas.width/2, canvas.height/2, 200, canvas.width/2, canvas.height/2, 500);
+gradient.addColorStop(0, '#0aff0a');
+gradient.addColorStop(0.6, 'yellow');
+gradient.addColorStop(1, 'green');
 
 class Symbol {
     constructor(x, y, fontSize, canvasHeight){
